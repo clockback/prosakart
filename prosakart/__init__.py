46f0d05c319c798833a120420b4fdaf2aa49641b
Elliot Paton-Simpson <tuxbert>
2019-11-11 17:16:40 +1300

    I think it may be only a problem with the snapcraft.yaml

--- prosakart/__init__.py
+++ prosakart/__init__.py
@@ -5,15 +5,15 @@ Tests Georgian vocabulary.
 # Import builtins.
 
 # Import local files.
-# from . sql_handle import SQLHandler
-# from . interfaces import MainWidget
-from . test import main
+from . sql_handle import SQLHandler
+from . interfaces import MainWidget
+# from . test import main
 
 __version__ = '0.1.0.0'
 
 # Runs the application.
-# handler: SQLHandler = SQLHandler()
-# widget: MainWidget = MainWidget(handler)
-# widget.top.mainloop()
-# handler.close()
-main()
+handler: SQLHandler = SQLHandler()
+widget: MainWidget = MainWidget(handler)
+widget.top.mainloop()
+handler.close()
+# main()
